8fd562c66b824ce0cd603825218bd76ba041046b
wine-fall <wine@mail.bnu.edu.cn>
2024-05-19 14:18:18 +0800

    feat: 首页loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,30 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {Suspense} from 'react';
+
+import {CircularProgress, Box, StyledEngineProvider} from '@mui/material';
+
 import './index.css';
 import './i18.ts';
 
+export default function CircularIndeterminate() {
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh'
+    }}>
+      <CircularProgress sx={{
+        marginTop: '400px'
+      }} />
+    </Box>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <StyledEngineProvider>
+    <Suspense fallback={<CircularIndeterminate />}>
+      <App />
+    </Suspense>
+  </StyledEngineProvider>,
 );
