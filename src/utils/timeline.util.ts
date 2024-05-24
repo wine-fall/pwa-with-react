b0c6b7b2cc19b976ca43d0164db88a3b2f8ad3e8
fawinell <fawinell@gmail.com>
2024-05-24 19:23:08 +0800

    feat: 更改时间轴配置

--- src/utils/timeline.util.ts
+++ src/utils/timeline.util.ts
@@ -19,13 +19,19 @@ export const createTimeLineOpts = (input: TimelineOptions): TimelineOptions => {
     zoomFriction: 5,
     zoomMax: twentyHundredYears,
     zoomMin: oneWeek,
+    groupHeightMode: 'fixed', // group 定高
     height: 400,
+    orientation: {
+      axis: 'both',
+      item: 'top', // 时间轴初始定位在顶部
+    },
+    start: '2019-03-03',
     ...input,
   };
 };
 
 export const createTimeLineGroup = (group: TimeLineVO[]) => {
-  const groups = group.map(({id, content}) => ({id, content}));
+  const groups = group.map(({id, content}) => ({id, content, order: id}));
   const items = [];
   for (const {itemList} of group) {
     for (const v of itemList) {
